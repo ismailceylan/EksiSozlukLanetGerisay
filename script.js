48c8b693511a3a549c9f15442ca4acaf6c38d54d
İsmail CEYLAN <ismaillceylan@gmail.com>
2020-04-09 14:17:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,135 @@
 
 ( function()
 {
-    'use strict';
+    "use strict";
+
+    var second2time = function( seconds, pattern, shortTerms, noZero )
+	{
+		if( shortTerms === undefined )
+        {
+			shortTerms = true;
+        }
+
+		if( noZero === undefined )
+        {
+			noZero = true;
+        }
+
+		if( Object.prototype.toString.call( seconds ) !== "[object Number]")
+        {
+			seconds = parseInt( seconds, 10 );
+        }
+
+		if( ! pattern )
+        {
+			pattern = "H:M:S";
+        }
+
+		var terms =
+		{
+			y: [ "yıl", "y" ],
+			mn: [ "ay", "a" ],
+			d: [ "gün", "g" ],
+			h: [ "saat", "sa" ],
+			m: [ "dakika", "dk" ],
+			s: [ "saniye", "sn" ]
+		}
+
+		var Years = 31557600;
+		var Months = 2629800;
+		var Days = 86400;
+		var Hours = 3600;
+		var Minutes = 60;
+
+		var years = Math.floor( seconds / Years );
+		var years_ = ( years * Years );
+		var months = Math.floor(( seconds - years_ ) / Months );
+		var months_ = ( months * Months );
+		var days = Math.floor(( seconds - years_ - months_ ) / Days );
+		var days_ = ( days * Days );
+		var hours = Math.floor(( seconds - years_ - months_ - days_ ) / Hours );
+		var hours_ = ( hours * Hours );
+		var minutes = Math.floor(( seconds - years_ - months_ - days_ - hours_ ) / Minutes );
+		var minutes_ = ( minutes * Minutes );
+ 		var secs = seconds - years_ - months_ - days_ - hours_ - minutes_;
+
+		var trimming = function( value )
+		{
+			if( noZero && value < 1 )
+            {
+				return "";
+            }
+
+			return value;
+		}
+
+		var naming = function( group, value )
+		{
+			if( ! value )
+            {
+				return value;
+            }
+
+			if( ! shortTerms )
+            {
+				value += " ";
+            }
+
+			return value + terms[ group ][ shortTerms? 1 : 0 ];
+		}
+
+		var padding = function( value )
+		{
+			return value.toString().padStart( 2, "0" );
+		}
+
+		var nt = function( group, value )
+		{
+			return naming( group, trimming( value ));
+		}
+
+		var np = function( group, value )
+		{
+			return naming( group, padding( value ));
+		}
+
+		var replacementMap =
+		[
+			[ "Y", nt( "y", years )],
+			[ "MN", nt( "mn", months )],
+			[ "D", nt( "d", days )],
+
+			[ /hh\*/i, np( "h", hours )],
+			[ /h\*/i, naming( "h", hours )],
+			[ /hh/i, padding( hours )],
+			[ /h/i, hours ],
+
+			[ /mm\*/, np( "m", minutes )],
+			[ /m\*/, naming( "m", minutes )],
+			[ /mm/, padding( minutes )],
+			[ /m/, minutes ],
+
+			[ /ss\*/, np( "s", secs )],
+			[ /s\*/, naming( "s", secs )],
+			[ /ss/, padding( secs )],
+			[ /s/, secs ],
+		];
+
+		var data = [];
+
+		replacementMap.forEach( function( item, i )
+		{
+			pattern = pattern.replace( item[ 0 ], "{#" + i + "}" );
+			data.push( item[ 1 ]);
+		});
+
+		data.forEach( function( value, i )
+		{
+			pattern = pattern.replace( "{#" + i + "}", value );
+		});
+
+		return pattern.trim();
+	}
 
     var styles = `
 @keyframes move
@@ -76,11 +204,13 @@
     {
         var start = new Date( "Mar 31 2020" );
         var end = new Date( "May 30 2020" );
+        var left = end - new Date;
+        var total = end - start;
 
-        var percent = (( end - new Date ) * 100 / ( end - start )).toFixed( 2 );
+        var percent = ( left * 100 / total ).toFixed( 2 );
 
         progress.find( "div" ).css( "width", percent + '%' );
-        progress.find( "span" ).text( '%' + percent );
+        progress.find( "span" ).text( second2time( Math.round( left / 1000 ), "Y MN D hh:mm:ss", false ) + " (%" + percent + ")" );
     },
     1200 );
 })();
